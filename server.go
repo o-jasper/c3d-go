c90387142d1be3a69f4d831537e71e5c105de1f0
Ethan Buchman <ethan@coinculture.info>
2014-07-03 02:48:26 -0400

    web app beginnings

---
+++ server.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+    "github.com/ethereum/eth-go/ethpub"
+    "github.com/ethereum/eth-go/ethutil"
+    "net/http"
+    "html/template"
+    "strings"
+    "log"
+)
+
+type account struct{
+    Addr string
+    Priv []byte
+    Value string
+    Nonce int   
+    Storage map[string]string // maps hex addrs to hex values
+    Code []byte
+}
+
+type torrent struct{
+    InfoHash string
+    Done bool
+    Contract string
+}
+
+type Session struct{
+    //Accounters map[string]int //map from addresses to account numbers
+   // Accounts map[int]account //map from account number to account
+    Accounts []account
+    AccountMap map[string]int //map from addr to account number
+    Contracts []account
+    Torrents []torrent
+    peth *ethpub.PEthereum
+}
+
+
+var templates = template.Must(template.ParseFiles("views/index.html", "views/config.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Session){
+    //we already parsed the html templates
+    err := templates.ExecuteTemplate(w, tmpl+".html", p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+func updateSession(s *Session){
+    for i:=0; i<len(s.Accounts);i++{
+        addr := (*s).Accounts[i].Addr
+        state := (*s).peth.GetStateObject(addr)
+        val := state.Value()
+        nonce := state.Nonce()
+        (*s).Accounts[i].Value = val
+        (*s).Accounts[i].Nonce = nonce
+    }
+}
+
+func loadSession(peth *ethpub.PEthereum) *Session {
+     keyRing := ethutil.GetKeyRing()
+     session := &Session{}
+     session.peth = peth
+     (*session).AccountMap = make(map[string]int)
+     for i:=0;i<keyRing.Len();i++{
+        key := keyRing.Get(i)
+        addr := ethutil.Hex(key.Address())
+        priv := key.PrivateKey
+        state := peth.GetStateObject(addr)
+        val := state.Value()
+        nonce := state.Nonce()
+        ac := account{Addr: addr, Value:val, Nonce:nonce, Priv:priv}
+        (*session).Accounts = append((*session).Accounts, ac)
+        (*session).AccountMap[addr] = i
+     }
+    return session
+}
+
+func (s *Session) handleTransact(w http.ResponseWriter, r *http.Request){
+        to := r.FormValue("recipient")
+        val := r.FormValue("amount")
+        g := r.FormValue("gas")
+        gp := r.FormValue("gasprice")
+        from := r.FormValue("from_addr")
+        acc_num := (*s).AccountMap[from]
+        priv := (*s).Accounts[acc_num].Priv
+        log.Println(to, val, g, gp, from, acc_num)
+        p, err := (*s).peth.Transact(ethutil.Hex(priv), to, val, g, gp, "")
+        if err != nil{
+            log.Println(err)
+        }
+        log.Println(p)
+        renderTemplate(w, "index", s)
+}
+
+func (s *Session) handleIndex(w http.ResponseWriter, r *http.Request){
+        updateSession(s)
+        renderTemplate(w, "index", s)
+}
+
+func (s *Session) serveFile(w http.ResponseWriter, r *http.Request){
+    if !strings.Contains(r.URL.Path, "."){
+        s.handleIndex(w, r)
+    }else{
+        http.ServeFile(w, r, r.URL.Path[1:])
+    }
+}
+
+func StartServer(peth *ethpub.PEthereum){
+    sesh := loadSession(peth)
+    http.HandleFunc("/views/", sesh.serveFile)
+    http.HandleFunc("/", sesh.handleIndex)
+    http.HandleFunc("/transact", sesh.handleTransact)
+    http.ListenAndServe(":9099", nil)
+}
